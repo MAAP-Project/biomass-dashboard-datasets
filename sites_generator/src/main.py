19badb589a8e1460cab4a395eb3057c98b97008d
Phil Varner <philvarner@gmail.com>
2021-09-23 09:51:01 -0400

    handle bucket already existing

--- sites_generator/src/main.py
+++ sites_generator/src/main.py
@@ -55,7 +55,7 @@ def create_sites_json():
                 'LocationConstraint': os.environ.get('AWS_REGION', 'us-east-1')
             }
         )
-    except botocore.errorfactory.BucketAlreadyOwnedByYou:
+    except Exception:
         # this exception is thrown if the bucket already exists in any region other than us-east-1
         pass
 
